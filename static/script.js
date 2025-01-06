7f0b1efa9a06248bdd4c55092225564966225d11
ianimbuga <imbugaian20@gmail.com>
2025-01-06 17:27:29 +0300

    complete project

---
+++ static/script.js
@@ -0,0 +1,9 @@
+// JavaScript to handle dynamic actions
+document.addEventListener('DOMContentLoaded', function () {
+    const admissionsSection = document.getElementById('admissions');
+
+    // Display an alert when the "Admissions" section is clicked
+    admissionsSection.addEventListener('click', function () {
+        alert('Applications are now open! Apply today.');
+    });
+});
